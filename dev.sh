c14028bc83f7db6543e1aa604836825b0ed5f48b
Esteban <esteban.sastre@tenforce.com>
2017-04-19 15:37:44 +0200

    Updated dev.sh

--- dev.sh
+++ dev.sh
@@ -2,10 +2,10 @@
 
 docker run --rm -it \
            -v "$PWD"/src:/app \
+           -v "$PWD"/containers:/usr/src/app/containers \
            -p 1234:80 \
            --link etmsplatform_db_1:database \
            --network etmsplatform_default \
            --name mu-docker-event-query \
            -e MODE=development \
            mu-docker-event-query
-
