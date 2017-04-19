f8bd545680c01625cada91cb57d9196289fb67e1
Esteban <esteban.sastre@tenforce.com>
2017-04-19 16:19:43 +0200

    Added minimal logging

--- src/web.py
+++ src/web.py
@@ -4,6 +4,7 @@ import json
 import signal
 import sys
 from time import time, sleep
+from helpers import log
 
 timenow = time()
 
@@ -11,6 +12,8 @@ def signal_handler(signal, frame):
     """
     Remove the containers information file if it exists upon recieving either SIGINT or SIGKILL.
     """
+    msg = "[+] Clean up containers file: {dir} {file}".format(dir=os.environ['WRITE_DIR'], file=os.environ['WRITE_FILE'])
+    log(msg)
     try:
         os.remove(os.environ['WRITE_DIR'] + os.environ['WRITE_FILE'])
     except OSError:
@@ -21,8 +24,13 @@ def main_loop():
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
 
+    log("[+] Entering main loop..")
+
     while True:
+        log("[+] Getting the list of running containers.")
         containers = queries.list_container_events(timenow)
+
+        log("[+] Writing the running containers into file.")
         queries.write_json_into_file(containers)
 
         sleep(int(os.environ['SLEEP_PERIOD']))
